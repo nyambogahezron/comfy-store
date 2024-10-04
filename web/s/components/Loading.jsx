d2ed759401c986aece0da91358ce5926e85467b7
nyambogahezron <hezzyn1@gmail.com>
2024-10-04 09:01:12 +0300

    Remove unnecessary configuration files and unused components

--- web/s/components/Loading.jsx
+++
@@ -1,8 +0,0 @@
-const Loading = () => {
-  return (
-    <div className='h-screen flex items-center justify-center'>
-      <span className='loading loading-ring loading-lg'></span>
-    </div>
-  );
-};
-export default Loading;
