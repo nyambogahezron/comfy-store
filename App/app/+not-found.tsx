970fdd57475b3eed26721fc589c05cc7f135fbd9
nyambogahezron <hezzyn1@gmail.com>
2024-10-04 09:30:14 +0300

    init setup

--- App/app/+not-found.tsx
+++ App/app/+not-found.tsx
@@ -1,17 +1,17 @@
 import { Link, Stack } from 'expo-router';
 import { StyleSheet } from 'react-native';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/Themed/ThemedText';
+import { ThemedView } from '@/components/Themed/ThemedView';
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+        <ThemedText type='title'>This screen doesn't exist.</ThemedText>
+        <Link href='/' style={styles.link}>
+          <ThemedText type='link'>Go to home screen!</ThemedText>
         </Link>
       </ThemedView>
     </>
--- App/hooks/useThemeColor.ts
+++ App/hooks/useThemeColor.ts
@@ -1,8 +1,3 @@
-/**
- * Learn more about light and dark modes:
- * https://docs.expo.dev/guides/color-schemes/
- */
-
 import { useColorScheme } from 'react-native';
 
 import { Colors } from '@/constants/Colors';
