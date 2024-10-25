2b5aff35bb2b8d8f5e624a419bb66c1c7082553c
nyambogahezron <hezzyn1@gmail.com>
2024-10-25 18:29:06 +0300

    add assets

--- App/src/constants/Colors.ts
+++ App/src/constants/Colors.ts
@@ -1,7 +1,4 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
+
 
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
@@ -24,3 +21,5 @@ export const Colors = {
     tabIconSelected: tintColorDark,
   },
 };
+
+// #06091b
