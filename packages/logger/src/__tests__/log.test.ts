04f6a90c96cf5dc34491aca0f04340e15bc88711
nyambogahezron <hezzyn1@gmail.com>
2025-11-09 14:45:17 +0300

    add packages

---
+++ packages/logger/src/__tests__/log.test.ts
@@ -0,0 +1,12 @@
+import { describe, it, expect, jest } from "@jest/globals";
+import { log } from "..";
+
+jest.spyOn(global.console, "log");
+
+describe("@repo/logger", () => {
+  it("prints a message", () => {
+    log("hello");
+    // eslint-disable-next-line no-console -- testing console
+    expect(console.log).toBeCalledWith("LOGGER: ", "hello");
+  });
+});
